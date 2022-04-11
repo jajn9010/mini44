1237a51fc74ec80692c4145fe6763684e6da604e
Eunah323 <unapretty@gmail.com>
2022-04-11 20:27:30 +0900

    302에러로 컴백. 핸들러부터 다시 만들어보자.

---
+++ src/main/java/com/sparta/miniproject/security/CustomAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.sparta.miniproject.security;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
+
+    public CustomAuthenticationFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        String userId = request.getParameter("userId");
+        String credentials = request.getParameter("password");
+
+        return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken(userId, credentials));
+    }
+}
