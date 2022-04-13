9a0ff96d99f296dbe9a174f82a416bed94486196
Eunah323 <unapretty@gmail.com>
2022-04-13 16:00:21 +0900

    일단 핸들러

---
+++ src/main/java/com/sparta/miniproject/security/RestAuthenticationFailureHandler.java
@@ -0,0 +1,35 @@
+package com.sparta.miniproject.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Map;
+
+@Component
+public class RestAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Autowired
+    UserResponseHandler userResponseHandlerHandler;
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse httpServletResponse,
+                                        AuthenticationException exception) throws IOException, ServletException {
+
+        httpServletResponse = userResponseHandlerHandler.setResponse(httpServletResponse);
+        Map<String,Object> response = userResponseHandlerHandler.setMessage("아이디와 비밀번호를 확인해 주세요.", HttpStatus.UNAUTHORIZED);
+
+        OutputStream out = httpServletResponse.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writerWithDefaultPrettyPrinter().writeValue(out, response);
+        out.flush();
+    };
+}
